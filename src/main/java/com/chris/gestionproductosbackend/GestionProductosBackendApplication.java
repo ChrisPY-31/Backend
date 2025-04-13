9c735be2140a848caa373c160993c9579971aae7
ChrisDev <Christianpeya@hotmail.com>
2025-04-12 23:44:51 -0600

    first commit

---
+++ src/main/java/com/chris/gestionproductosbackend/GestionProductosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chris.gestionproductosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProductosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionProductosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chris/gestionproductosbackend/GestionProductosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.gestionproductosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProductosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
